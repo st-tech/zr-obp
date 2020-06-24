3c075d391be201403691d6c6f031632c44ef52f8
usaito <elpistolero317@gmail.com>
2020-06-24 13:21:10 +0900

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../obp'))
-
+# sys.path.insert(0, os.path.abspath('../obp'))
 
 # -- Project information -----------------------------------------------------
 
@@ -27,6 +26,15 @@ author = 'Yuta Saito'
 version = 'latest'
 # The full version, including alpha/beta/rc tags
 release = 'latest'
+
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
+sys.path.insert(0, project_root)
 
 
 # -- General configuration ---------------------------------------------------
