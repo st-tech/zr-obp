1e8e08181919c8a594bda42225e5b4cd4ac75b74
usaito <elpistolero317@gmail.com>
2020-07-04 19:17:49 +0900

    implement some properties and bootstrap sampling

--- obp/dataset.py
+++ obp/dataset.py
@@ -5,15 +5,16 @@
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Tuple, Dict, Union
+from typing import Tuple, Dict, Union, Optional
 
 import numpy as np
 import pandas as pd
 from scipy.stats import rankdata
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.utils import check_random_state
 
-LogBanditFeedback = Dict[str, Union[str, np.ndarray]]
+BanditFeedback = Dict[str, Union[str, np.ndarray]]
 
 
 class BaseBanditDataset(metaclass=ABCMeta):
@@ -72,18 +73,64 @@ class OpenBanditDataset(BaseBanditDataset):
         self.load_raw_data()
         self.pre_process()
 
-        self.n_rounds = self.action.shape[0]
-        self.n_actions = self.action.max() + 1
-        self.dim_context = self.context.shape[1]
+    @property
+    def n_rounds(self) -> int:
+        """Total number of rounds in the dataset."""
+        return self.data.shape[0]
+
+    @property
+    def n_actions(self) -> int:
+        """Number of actions."""
+        return self.action.max() + 1
+
+    @property
+    def dim_context(self) -> int:
+        """Dimension of context vectors."""
+        return self.context.shape[1]
+
+    @property
+    def len_list(self) -> int:
+        """Length of recommendation lists."""
+        return self.position.max() + 1
+
+    @classmethod
+    def calc_on_policy_policy_value_estimate(cls,
+                                             behavior_policy: str,
+                                             campaign: str,
+                                             data_path: Path = Path('./obd')) -> float:
+        """Calculate on-policy  (used as a ground-truth policy value).
+
+        Parameters
+        ----------
+        behavior_policy: str
+            Name of the behavior policy that generated the log data.
+            Must be 'random' or 'bts'.
+
+        campaign: str
+            One of the three possible campaigns (i.e., "all", "men", and "women").
+
+        data_path: Path, default: Path('./obd')
+            Path that stores Open Bandit Dataset.
+
+        Returns
+        ---------
+        on_policy_policy_value_estimate: float
+            Estimated on-policy policy value of behavior policy, i.e., :math:`T^{-1} \\sum_{t=1}^T Y_t`.
+            This parameter is used as a ground-truth policy value in the evaluation of OPE estimators.
+
+        """
+        return cls(
+            behavior_policy=behavior_policy,
+            campaign=campaign,
+            data_path=data_path
+        ).reward.mean()
 
     def load_raw_data(self) -> None:
         """Load raw open bandit dataset."""
-
         self.data = pd.read_csv(self.data_path / self.raw_data_file, index_col=0)
         self.data.sort_values('timestamp', inplace=True)
         self.action = self.data['item_id'].values
-        self.pos = (rankdata(self.data['position'].values, 'dense') - 1).astype(int)
-        self.len_list = self.pos.max() + 1
+        self.position = (rankdata(self.data['position'].values, 'dense') - 1).astype(int)
         self.reward = self.data['click'].values
         self.pscore = self.data['propensity_score'].values
 
@@ -96,10 +143,55 @@ class OpenBanditDataset(BaseBanditDataset):
         item_feature_cat = item_context.drop('item_feature_0', 1).apply(LabelEncoder().fit_transform)
         self.action_context = pd.concat([item_feature_cat, item_feature_0], 1).values
 
+    def obtain_batch_bandit_feedback(self) -> BanditFeedback:
+        """Obtain batch logged bandit feedback."""
+        return dict(
+            n_rounds=self.n_rounds,
+            n_actions=self.n_actions,
+            action=self.action,
+            position=self.position,
+            reward=self.reward,
+            pscore=self.pscore,
+            context=self.context,
+            action_context=self.action_context
+        )
+
+    def sample_bootstrap_bandit_feedback(self, random_state: Optional[int] = None) -> BanditFeedback:
+        """Sample bootstrap logged bandit feedback.
+
+        Parameters
+        -----------
+        random_state: int, default: None
+            Controls the random seed in sampling actions.
+
+        Returns
+        --------
+        bootstrap_bandit_feedback: BanditFeedback
+            Bootstrapped logged bandit feedback independently sampled from the original data with replacement.
+
+        """
+        random_ = check_random_state(random_state)
+        bootstrap_idx = random_.choice(
+            np.arange(self.n_rounds),
+            size=self.n_rounds,
+            replace=True
+        )
+
+        return dict(
+            n_rounds=self.n_rounds,
+            n_actions=self.n_actions,
+            action=self.action[bootstrap_idx],
+            position=self.position[bootstrap_idx],
+            reward=self.reward[bootstrap_idx],
+            pscore=self.pscore[bootstrap_idx],
+            context=self.context[bootstrap_idx, :],
+            action_context=self.action_context
+        )
+
     def split_data(self,
                    test_size: Union[int, float] = 0.3,
                    is_timeseries_split: bool = False,
-                   random_state: int = 0) -> Tuple[LogBanditFeedback, LogBanditFeedback]:
+                   random_state: int = 0) -> Tuple[BanditFeedback, BanditFeedback]:
         """Split dataset into training and test sets.
 
         Parameters
@@ -116,17 +208,17 @@ class OpenBanditDataset(BaseBanditDataset):
 
         Returns:
         ----------
-        train: LogBanditFeedback
+        train: BanditFeedback
             Dictionary storing the training set after preprocessing.
 
-        test: LogBanditFeedback
+        test: BanditFeedback
             Dictionary storing the test set after preprocessing.
         """
         if is_timeseries_split:
             test_size = test_size if isinstance(test_size, int) else np.int(test_size * self.n_rounds)
             train_size = np.int(self.n_rounds - test_size)
             action_train, action_test = self.action[:train_size], self.action[train_size:]
-            pos_train, pos_test = self.pos[:train_size], self.pos[train_size:]
+            pos_train, pos_test = self.position[:train_size], self.position[train_size:]
             reward_train, reward_test = self.reward[:train_size], self.reward[train_size:]
             pscore_train, pscore_test = self.pscore[:train_size], self.pscore[train_size:]
             context_train, context_test = self.context[:train_size], self.context[train_size:]
@@ -135,12 +227,13 @@ class OpenBanditDataset(BaseBanditDataset):
                 pscore_train, pscore_test, context_train, context_test =\
                 train_test_split(
                     self.action,
-                    self.pos,
+                    self.position,
                     self.reward,
                     self.pscore,
                     self.context,
                     test_size=test_size,
-                    random_state=random_state)
+                    random_state=random_state
+                )
 
         self.train_size = action_train.shape[0]
         self.test_size = action_test.shape[0]
@@ -152,7 +245,8 @@ class OpenBanditDataset(BaseBanditDataset):
             reward=reward_train,
             pscore=pscore_train,
             context=context_train,
-            action_context=self.action_context)
+            action_context=self.action_context
+        )
         test = dict(
             n_rounds=self.test_size,
             n_actions=self.n_actions,
@@ -161,6 +255,7 @@ class OpenBanditDataset(BaseBanditDataset):
             reward=reward_test,
             pscore=pscore_test,
             context=context_test,
-            action_context=self.action_context)
+            action_context=self.action_context
+        )
 
         return train, test
