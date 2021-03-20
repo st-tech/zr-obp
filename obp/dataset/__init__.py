81b79b318c11c24b67f387a074ba3d2f2601ddd8
nmasahiro <masahironomura5325@gmail.com>
2021-03-20 12:53:19 +0900

    fix flake8 error for simulation/__init__.py

--- obp/dataset/__init__.py
+++ obp/dataset/__init__.py
@@ -2,6 +2,9 @@ from obp.dataset.base import BaseBanditDataset
 from obp.dataset.base import BaseRealBanditDataset
 from obp.dataset.real import OpenBanditDataset
 from obp.dataset.synthetic import SyntheticBanditDataset
+from obp.dataset.synthetic import logistic_reward_function
+from obp.dataset.synthetic import linear_reward_function
+from obp.dataset.synthetic import linear_behavior_policy
 from obp.dataset.multiclass import MultiClassToBanditReduction
 
 
@@ -10,5 +13,8 @@ __all__ = [
     "BaseRealBanditDataset",
     "OpenBanditDataset",
     "SyntheticBanditDataset",
+    "logistic_reward_function",
+    "linear_reward_function",
+    "linear_behavior_policy",
     "MultiClassToBanditReduction",
 ]
