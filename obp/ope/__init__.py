498fa285bc421f9948235732a25bfb7b9b1ad7f5
nmasahiro <masahironomura5325@gmail.com>
2021-03-20 12:34:16 +0900

    fix flake8 error for ope/__init__.py

--- obp/ope/__init__.py
+++ obp/ope/__init__.py
@@ -1,6 +1,14 @@
-from .estimators import *
-from .meta import *
-from .regression_model import *
+from obp.ope.estimators import BaseOffPolicyEstimator
+from obp.ope.estimators import ReplayMethod
+from obp.ope.estimators import InverseProbabilityWeighting
+from obp.ope.estimators import SelfNormalizedInverseProbabilityWeighting
+from obp.ope.estimators import DirectMethod
+from obp.ope.estimators import DoublyRobust
+from obp.ope.estimators import SelfNormalizedDoublyRobust
+from obp.ope.estimators import SwitchDoublyRobust
+from obp.ope.estimators import DoublyRobustWithShrinkage
+from obp.ope.meta import OffPolicyEvaluation
+from obp.ope.regression_model import RegressionModel
 
 __all_estimators__ = [
     "ReplayMethod",
