d5d107003f11ed37d1e4ff834cee02137e085278
usaito <elpistolero317@gmail.com>
2021-10-29 23:53:31 -0400

    implement the gumble softmax trick to sample rankings of actions

--- obp/policy/offline.py
+++ obp/policy/offline.py
@@ -29,7 +29,6 @@ from obp.ope import RegressionModel
 from ..utils import check_array
 from ..utils import check_bandit_feedback_inputs
 from ..utils import check_tensor
-from ..utils import sample_action_fast
 from ..utils import softmax as softmax_axis1
 from .base import BaseOfflinePolicyLearner
 
@@ -251,25 +250,23 @@ class IPWLearner(BaseOfflinePolicyLearner):
         tau: Union[int, float] = 1.0,
         random_state: Optional[int] = None,
     ) -> np.ndarray:
-        """Sample (non-repetitive) actions based on scores predicted by a classifier.
+        """Sample a ranking of (non-repetitive) actions from the Plackett-Luce ranking distribution.
 
         Note
         --------
-        This `sample_action` method samples a **non-repetitive** set of actions for new data :math:`x \\in \\mathcal{X}`
-        by first computing non-negative scores for all possible candidate products of action and position
-        :math:`(a, k) \\in \\mathcal{A} \\times \\mathcal{K}` (where :math:`\\mathcal{A}` is an action set and
-        :math:`\\mathcal{K}` is a position set), and using softmax function as follows:
+        This `sample_action` method samples a **non-repetitive** ranking of actions for new data
+        :math:`x \\in \\mathcal{X}` via the so-colled "Gumbel Softmax trick" as follows.
 
         .. math::
 
-            & P (A_1 = a_1 | x) = \\frac{\\mathrm{exp}(f(x,a_1,1) / \\tau)}{\\sum_{a^{\\prime} \\in \\mathcal{A}} \\mathrm{exp}( f(x,a^{\\prime},1) / \\tau)} , \\\\
-            & P (A_2 = a_2 | A_1 = a_1, x) = \\frac{\\mathrm{exp}(f(x,a_2,2) / \\tau)}{\\sum_{a^{\\prime} \\in \\mathcal{A} \\backslash \\{a_1\\}} \\mathrm{exp}(f(x,a^{\\prime},2) / \\tau )} ,
-            \\ldots
+            \\s (x,a) = \\hat{f}(x,a) / \\tau + \\gamma_{x,a}, \\quad \\gamma_{x,a} \\sim \\mathrm{Gumbel}(0,1)
 
-        where :math:`A_k` is a random variable representing an action at a position :math:`k`.
         :math:`\\tau` is a temperature hyperparameter.
         :math:`f: \\mathcal{X} \\times \\mathcal{A} \\times \\mathcal{K} \\rightarrow \\mathbb{R}_{+}`
         is a scoring function which is now implemented in the `predict_score` method.
+        :math:`\\gamma_{x,a}` is a random variable sampled from the Gumbel distribution.
+        By sorting the actions based on :math:`\\s (x,a)` for each context, we can sample a ranking from
+        the Plackett-Luce ranking distribution.
 
         Parameters
         ----------------
@@ -286,7 +283,7 @@ class IPWLearner(BaseOfflinePolicyLearner):
         Returns
         -----------
         action: array-like, shape (n_rounds_of_new_data, n_actions, len_list)
-            Action sampled by a trained classifier.
+            Ranking of actions sampled by the Gumbel softmax trick as follows.
 
         """
         check_array(array=context, name="context", expected_dim=2)
@@ -294,16 +291,13 @@ class IPWLearner(BaseOfflinePolicyLearner):
 
         n_rounds = context.shape[0]
         random_ = check_random_state(random_state)
-        action = np.zeros((n_rounds, self.n_actions, self.len_list))
-        score_predicted = self.predict_score(context=context)
-        for i in tqdm(np.arange(n_rounds), desc="[sample_action]", total=n_rounds):
-            action_set = np.arange(self.n_actions)
-            for position_ in np.arange(self.len_list):
-                score_ = softmax(score_predicted[i, action_set, position_] / tau)
-                action_sampled = random_.choice(action_set, p=score_, replace=False)
-                action[i, action_sampled, position_] = 1
-                action_set = np.delete(action_set, action_set == action_sampled)
-        return action
+        sampled_action = np.zeros((n_rounds, self.n_actions, self.len_list))
+        scores = self.predict_score(context=context).mean(2) / tau
+        scores += random_.gumbel(size=scores.shape)
+        ranking = np.argsort(-scores, axis=1)
+        for position_ in np.arange(self.len_list):
+            sampled_action[np.arange(n_rounds), ranking[:, position_], position_] = 1
+        return sampled_action
 
     def predict_proba(
         self,
@@ -315,9 +309,7 @@ class IPWLearner(BaseOfflinePolicyLearner):
         Note
         --------
         This `predict_proba` method obtains action choice probabilities for new data :math:`x \\in \\mathcal{X}`
-        by first computing non-negative scores for all possible candidate actions
-        :math:`a \\in \\mathcal{A}` (where :math:`\\mathcal{A}` is an action set),
-        and using a Plackett-Luce ranking model as follows:
+        by applying the softmax function as follows:
 
         .. math::
 
@@ -476,6 +468,9 @@ class QLearner(BaseOfflinePolicyLearner):
         where :math:`\\hat{q}(x,a)` is an estimator for the q function :math:`\\q(x,a) := \\mathbb{E} [r \\mid x, a]`.
         Note that the action set predicted by this `predict` method can contain duplicate items.
 
+        Action set predicted by this `predict` method can contain duplicate items.
+        If you want a non-repetitive action set, then please use the `sample_action` method.
+
         Parameters
         -----------
         context: array-like, shape (n_rounds_of_new_data, dim_context)
@@ -529,19 +524,23 @@ class QLearner(BaseOfflinePolicyLearner):
         tau: Union[int, float] = 1.0,
         random_state: Optional[int] = None,
     ) -> np.ndarray:
-        """Sample actions based on the estimated expected rewards.
+        """Sample a ranking of (non-repetitive) actions from the Plackett-Luce ranking distribution.
 
         Note
         --------
-        This `sample_action` method samples a set of actions for new data based on :math:`\\hat{q}` as follows.
+        This `sample_action` method samples a ranking of (non-repetitive) actions for new data
+        based on :math:`\\hat{q}` and the so-colled "Gumbel Softmax trick" as follows.
 
         .. math::
 
-            \\pi (a | x) = \\frac{\\mathrm{exp}( \\hat{q}(x,a) / \\tau)}{\\sum_{a^{\\prime} \\in \\mathcal{A}} \\mathrm{exp}( \\hat{q}(x,a^{\\prime}) / \\tau)}
+            \\s (x,a) = \\hat{q}(x,a) / \\tau + \\gamma_{x,a}, \\quad \\gamma_{x,a} \\sim \\mathrm{Gumbel}(0,1)
 
         :math:`\\tau` is a temperature hyperparameter.
         :math:`\\hat{q}: \\mathcal{X} \\times \\mathcal{A} \\times \\mathcal{K} \\rightarrow \\mathbb{R}_{+}`
         is a q function estimator, which is now implemented in the `predict_score` method.
+        :math:`\\gamma_{x,a}` is a random variable sampled from the Gumbel distribution.
+        By sorting the actions based on :math:`\\s (x,a)` for each context, we can sample a ranking from
+        the Plackett-Luce ranking distribution.
 
         Parameters
         ----------------
@@ -557,25 +556,22 @@ class QLearner(BaseOfflinePolicyLearner):
 
         Returns
         -----------
-        action: array-like, shape (n_rounds_of_new_data, n_actions, len_list)
-            Action sampled based on the estimated expected rewards.
+        sampled_action: array-like, shape (n_rounds_of_new_data, n_actions, len_list)
+            Ranking of action sampled from the Plackett-Luce ranking distribution by the Gumbel softmax trick.
 
         """
-        base_action_dist = self.predict_proba(context=context, tau=tau)
+        check_array(array=context, name="context", expected_dim=2)
+        check_scalar(tau, name="tau", target_type=(int, float), min_val=0)
 
         n_rounds = context.shape[0]
-        action_dist = np.zeros_like(base_action_dist)
-        for p in np.arange(self.len_list):
-            sampled_action = sample_action_fast(
-                base_action_dist[:, :, p], random_state=random_state
-            )
-            action_dist[
-                np.arange(n_rounds),
-                sampled_action,
-                np.ones(n_rounds, dtype=int) * p,
-            ] = 1
-
-        return action_dist
+        random_ = check_random_state(random_state)
+        sampled_action = np.zeros((n_rounds, self.n_actions, self.len_list))
+        scores = self.predict_score(context=context).mean(2) / tau
+        scores += random_.gumbel(size=scores.shape)
+        ranking = np.argsort(-scores, axis=1)
+        for position_ in np.arange(self.len_list):
+            sampled_action[np.arange(n_rounds), ranking[:, position_], position_] = 1
+        return sampled_action
 
     def predict_proba(
         self,
@@ -631,7 +627,6 @@ class NNPolicyLearner(BaseOfflinePolicyLearner):
     len_list: int, default=1
         Length of a list of actions recommended in each impression.
         When Open Bandit Dataset is used, 3 should be set.
-        Currently, len_list > 1 is not supported.
 
     dim_context: int
         Number of dimensions of context vectors.
@@ -769,9 +764,6 @@ class NNPolicyLearner(BaseOfflinePolicyLearner):
     def __post_init__(self) -> None:
         """Initialize class."""
         super().__post_init__()
-
-        if self.len_list != 1:
-            raise NotImplementedError("currently, len_list > 1 is not supported")
 
         check_scalar(self.dim_context, "dim_context", int, min_val=1)
 
@@ -1044,8 +1036,6 @@ class NNPolicyLearner(BaseOfflinePolicyLearner):
             pscore = np.ones_like(action) / self.n_actions
         if self.len_list == 1:
             position = np.zeros_like(action, dtype=int)
-        else:
-            raise NotImplementedError("currently, len_list > 1 is not supported")
 
         # train q function estimator when it is needed to train NNPolicy
         if self.off_policy_objective != "ipw":
@@ -1291,25 +1281,47 @@ class NNPolicyLearner(BaseOfflinePolicyLearner):
     def sample_action(
         self,
         context: np.ndarray,
+        tau: Union[int, float] = 1.0,
         random_state: Optional[int] = None,
     ) -> np.ndarray:
-        """Sample (non-repetitive) actions based on action choice probabilities.
+        """Sample a ranking of (non-repetitive) actions from the Plackett-Luce ranking distribution.
+
+        Note
+        --------
+        This `sample_action` method samples a **non-repetitive** ranking of actions for new data
+        :math:`x \\in \\mathcal{X}` via the so-colled "Gumbel Softmax trick" as follows.
+
+        .. math::
+
+            \\s (x,a) = \\hat{f}(x,a) / \\tau + \\gamma_{x,a}, \\quad \\gamma_{x,a} \\sim \\mathrm{Gumbel}(0,1)
+
+        :math:`\\tau` is a temperature hyperparameter.
+        :math:`f: \\mathcal{X} \\times \\mathcal{A} \\times \\mathcal{K} \\rightarrow \\mathbb{R}_{+}`
+        is a scoring function which is now implemented in the `predict_score` method.
+        :math:`\\gamma_{x,a}` is a random variable sampled from the Gumbel distribution.
+        By sorting the actions based on :math:`\\s (x,a)` for each context, we can sample a ranking from
+        the Plackett-Luce ranking distribution.
 
         Parameters
         ----------------
         context: array-like, shape (n_rounds_of_new_data, dim_context)
             Context vectors for new data.
 
+        tau: int or float, default=1.0
+            A temperature parameter, controlling the randomness of the action choice.
+            As :math:`\\tau \\rightarrow \\infty`, the algorithm will select arms uniformly at random.
+
         random_state: int, default=None
             Controls the random seed in sampling actions.
 
         Returns
         -----------
-        action: array-like, shape (n_rounds_of_new_data, n_actions, len_list)
-            Action sampled by a trained classifier.
+        sampled_action: array-like, shape (n_rounds_of_new_data, n_actions, len_list)
+            Ranking of action sampled from the Plackett-Luce ranking distribution by the Gumbel softmax trick.
 
         """
         check_array(array=context, name="context", expected_dim=2)
+        check_scalar(tau, name="tau", target_type=(int, float), min_val=0)
         if context.shape[1] != self.dim_context:
             raise ValueError(
                 "Expected `context.shape[1] == self.dim_context`, but found it False"
@@ -1317,16 +1329,13 @@ class NNPolicyLearner(BaseOfflinePolicyLearner):
 
         n_rounds = context.shape[0]
         random_ = check_random_state(random_state)
-        action = np.zeros((n_rounds, self.n_actions, self.len_list))
-        score_predicted = self.predict_proba(context=context)
-        for i in tqdm(np.arange(n_rounds), desc="[sample_action]", total=n_rounds):
-            action_set = np.arange(self.n_actions)
-            for position_ in np.arange(self.len_list):
-                score_ = score_predicted[i, action_set, position_]
-                action_sampled = random_.choice(action_set, p=score_, replace=False)
-                action[i, action_sampled, position_] = 1
-                action_set = np.delete(action_set, action_set == action_sampled)
-        return action
+        sampled_action = np.zeros((n_rounds, self.n_actions, self.len_list))
+        scores = self.predict_proba(context=context).mean(2) / tau
+        scores += random_.gumbel(size=scores.shape)
+        ranking = np.argsort(-scores, axis=1)
+        for position_ in np.arange(self.len_list):
+            sampled_action[np.arange(n_rounds), ranking[:, position_], position_] = 1
+        return sampled_action
 
     def predict_proba(
         self,
